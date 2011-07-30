24fd2f98616850752847fbbcfeb6a1037547a5d3
jtowle <jeff@urbanairship.com>
2011-07-29 17:57:52 -0700

    Initial Commit

---
+++ UAPhoneGap/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  UAPhoneGap
+//
+//  Created by Jeff Towle on 7/29/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
